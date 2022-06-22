822bb57e423dc5fd9d0dbd88494195c5f9390f9b
Tomasz <skipperro@gmail.com>
2022-06-22 20:58:13 +0200

    added listener to init

--- custom_components/ha_test_integration/config_flow.py
+++ custom_components/ha_test_integration/config_flow.py
@@ -32,7 +32,7 @@ class CustomFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     @staticmethod
     @callback
-    def async_get_options_flow(config_entry):
+    async def async_get_options_flow(config_entry):
         """Get the options flow for this handler."""
         return OptionsFlowHandler(config_entry)
 
