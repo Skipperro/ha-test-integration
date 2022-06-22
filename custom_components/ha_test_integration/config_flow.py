3de2411900ecee59372b505f977b653c97e25b91
Tomasz <skipperro@gmail.com>
2022-06-22 20:44:39 +0200

    added configuration framework

--- custom_components/ha_test_integration/config_flow.py
+++ custom_components/ha_test_integration/config_flow.py
@@ -8,7 +8,7 @@ import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 
 from homeassistant import config_entries, exceptions
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 
 from .const import DOMAIN  # pylint:disable=unused-import
 
@@ -23,6 +23,28 @@ class CustomFlow(config_entries.ConfigFlow, domain=DOMAIN):
     data: Optional[Dict[str, Any]]
 
     async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
+        errors: Dict[str, str] = {}
+        if user_input is not None:
+            self.data = user_input
+            return self.async_create_entry(title="Test Integration", data=self.data)
+
+        return self.async_show_form(step_id="user", data_schema=CONFIG_SCHEMA, errors=errors)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return OptionsFlowHandler(config_entry)
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Handles options flow for the component."""
+
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        self.config_entry = config_entry
+
+    async def async_step_user(
+        self, user_input: Dict[str, Any] = None
+    ) -> Dict[str, Any]:
         errors: Dict[str, str] = {}
         if user_input is not None:
             self.data = user_input
